e45f899461b471fef0ae7a08035b21b091f5d88c
vortex <vortex>
2001-04-01 02:11:20 +0000

    Starting to actually write the libraries

--- SocketObject/SocketAttribute.cpp
+++ SocketObject/SocketAttribute.cpp
@@ -5,9 +5,32 @@ using namespace std;
 
 namespace archendale 
 {
-	SocketAttribute::SocketAttribute()
+	SocketAttribute::SocketAttribute(unsigned int port, const String& host = "localhost")
 	{
-		cout << "SocketAttribute Constructor Stub" << endl;
+		cout << "SocketAttribute(port, host) Constructor Stub" << endl;
 	} // SocketAttribute
 
+	SocketAttribute::SocketAttribute(const String& portName, const String& host = "localhost")
+	{
+		cout << "SocketAttribute(portName, host) Constructor Stub" << endl;
+	} // SocketAttribute(portName, host)
+
+	~SocketAttribute::SocketAttribute() 
+	{
+		cout << "~SocketAttribute" << endl;
+	} // ~SocketAttribute
+
+	// setPort
+	//      sets the port the host will listen on
+	void setPort(unsigned int)
+	{
+		cout << "setPort" << endl;
+	} // setPort
+
+	// setPort
+	//      sets the port the host will listen on by a port name
+	void setPort(const String& portName)
+	{
+		cout << "setPort" << endl;
+	} // setPort
 } // archendale
--- SocketObject/SocketAttribute.h
+++ SocketObject/SocketAttribute.h
@@ -4,14 +4,35 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>    
+#include <String/String.h>    
 
 namespace archendale
 {
 	class SocketAttribute 
 	{
 	public:
-		SocketAttribute();
+		// SocketAttribute Constructores:
+		//	Constructer for a Socket Attribute
+		//	Format:
+		//	SocketAttribute(hoststring /* "host:port" */)
+		//	SocketAttribute(port, host)
+		//	SocketAttribute(portName, host)
+		//
+		SocketAttribute(int, const String& = "localhost");
+		SocketAttribute(const String&, const String& = "localhost");
+		virtual ~SocketAttribute();
+
+		// setHostName:
+		//	sets the host name that the socket listens on
+		void setHostName();
+
+		// setPort
+		//	sets the port the host will listen on
+		void setPort(unsigned int);
+		void setPort(const String&);
 	private:
+		String m_hostName;
+		unsigned int m_portNumber;
 	}; // SocketAttribute
 }; // archendale
 
